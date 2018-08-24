7a18a15348f761893d1313b7d693e86fb9c6c7f6
Brad McCollum <btmcco8@gmail.com>
2018-08-24 21:41:18 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -10,8 +10,8 @@ class Genre
     @@all << self
   end
 
-  def new_song(name, artist)
-    binding.pry
+  def new_song(name, artist, genre)
+    # binding.pry
     song = Song.new(name, artist, self)
     @@all << song
   end
@@ -30,4 +30,4 @@ class Genre
   end
 
 end
-binding.pry 
+binding.pry
