bfb4c82daf39fb6125863bd5e3f473796699ada6
Brad McCollum <btmcco8@gmail.com>
2018-08-24 21:20:33 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,8 +3,14 @@ class Song
 
   @@all = []
 
-  def initialize(name)
+  def initialize(name, artist, genre)
     @name = name
+    @artist = artist
+    @genre = genre
     @@all << self
+  end
+
+  def self.all
+    @@all
   end
 end
