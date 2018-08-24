fc7ebb1199f772b395ffddbf64e5354783acb6e1
Brad McCollum <btmcco8@gmail.com>
2018-08-24 21:18:39 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -8,9 +8,20 @@ class Artist
     @@all << self
   end
 
-  #takes in a name and genre and creates a new song for the artist
+  #takes in a song name and genre and creates a new song for the artist
   def add_song(name, genre)
+    song = Song.new(name)
+    song.artist = self
+    song.genre = genre
+    @songs << song
+  end
 
+  def songs
+    Song.all.select {|songs| songs.artist == self}
+  end
+
+  def self.genres
+    @songs.map{|song_name| song_name.genre}.uniq
   end
 
   def self.all
